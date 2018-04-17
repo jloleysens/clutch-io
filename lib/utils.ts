e9b75c3c2c9dd7ffaeaed81d59160b6cb7dcc831
Jean-Louis Leysens <jloleysens@gmail.com>
2018-04-17 18:42:01 +0200

    Clean up

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,7 +2,3 @@ export const is = {
   function: val => typeof val == 'function',
   promise: val => val && is.function(val.then),
 }
-
-const infiniObject = new Proxy({}, {get() {
-  return infiniObject;
-}});
